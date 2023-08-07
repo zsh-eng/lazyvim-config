420c2e8cb6007240f26dfb20d932e316170d5ab4
sheng <chengzhisheng2001@gmail.com>
2023-08-07 17:20:44 +0800

    Add black formatter

---
+++ lua/plugins/python.lua
@@ -0,0 +1,19 @@
+-- Adds black formatter to config
+return {
+  -- Add `pyright` to mason
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "pyright", "black" })
+    end,
+  },
+
+  -- Setup null-ls with `black`
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, { nls.builtins.formatting.black })
+    end,
+  },
+}
